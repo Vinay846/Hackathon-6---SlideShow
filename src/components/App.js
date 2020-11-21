546bc90dc65ff0a1f638f7e819e0fd38a32d0129
Vinay846 <Vinayvin84629@gmail.com>
2020-11-22 01:40:11 +0530

    button disabled

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import '../styles/App.css';
 
 const App = (props) => {
   const [next, setNext] = useState(0);
+  let len = props.slides.length-2;
 
   const nextButton =()=>{
-    let len = props.slides.length-2;
     console.log(len);
     (next <= len)
     ? setNext(next+1):len
@@ -18,9 +18,9 @@ const App = (props) => {
     <>
     <h1 data-testid="title">{props.slides[next]["title"]}</h1>
     <p data-testid="text">{props.slides[next]["text"]}</p>
-    <button onClick={prevButton} data-testid="button-prev">Prev</button>
-    <button onClick={nextButton} data-testid="button-next">Next</button>
-    <button onClick={()=>{setNext(0)}} data-testid="button-restart">Restart</button>
+    <button disabled={next <= 0} onClick={prevButton} data-testid="button-prev">Prev</button>
+    <button disabled={next >= len} onClick={nextButton} data-testid="button-next">Next</button>
+    <button disabled={next <= 0 || next >= len} onClick={()=>{setNext(0)}} data-testid="button-restart">Restart</button>
     </>
   )
 }
